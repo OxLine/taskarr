9076243897431764974199a8c0d73b4bbc787893
chernyshof <dmitriy.chernyshof@gmail.com>
2017-03-30 20:56:19 +0300

    another small fix for heroku...

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :taskarr, Taskarr.Web.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :hello_phoenix, HelloPhoenix.Repo,
+config :taskarr, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
