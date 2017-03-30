9ab006fba7152ac63f7ca28c561081e6c7f473df
chernyshof <dmitriy.chernyshof@gmail.com>
2017-03-30 20:50:03 +0300

    another heroku deploy fix...

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Taskarr.Mixfile do
   def project do
     [app: :taskarr,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.3.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
